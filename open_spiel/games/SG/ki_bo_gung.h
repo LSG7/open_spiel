f71d3bb7a67c6ad549078bda2d46bb0fc173c8a8
Lee Sang-Gun <lsg8707@naver.com>
2023-08-30 14:17:36 +0900

    Update ki_bo_gung.h

--- open_spiel/games/SG/ki_bo_gung.h
+++ open_spiel/games/SG/ki_bo_gung.h
@@ -1,1 +1,23 @@
+##ifndef OPEN_SPIEL_GAMES_KI_BO_GUNG
+#define OPEN_SPIEL_GAMES_KI_BO_GUNG
 
+/**
+* @file ki_bo_gung.h
+* @brief [기병 보병 궁병] '3가지 병' 으로 '두 플레이어' 가 싸우는 'inperfect information' 'zero sum' 게임 이다.
+* @author SG Lee
+* @date 8/30/2023
+*/
+
+// STD headers
+
+// internal headers
+#include "open_spiel/spiel.h"
+
+namespace open_spiel {
+namespace kbg {
+
+inline constexpr int kNumPlayers = 2;
+inline constexpr int kNumRows = 10;
+inline constexpr int kNumCols = 10;
+inline constexpr int kNumCelss = kNumRows * kNumCols;
+inline constexpr int kCellStates = TODO
