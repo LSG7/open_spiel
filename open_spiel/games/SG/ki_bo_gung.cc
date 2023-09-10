30ed5942ef73481650d61bcb02f0ef3c4451b93f
SanggunLee <lsg8707@naver.com>
2023-09-10 20:25:03 +0900

    GameType 설정 중
    Signed-off-by: SanggunLee <lsg8707@naver.com>

--- open_spiel/games/SG/ki_bo_gung.cc
+++ open_spiel/games/SG/ki_bo_gung.cc
@@ -4,19 +4,21 @@
 namespace open_spiel {
 namespace  kbg {
 namespace {
+
 // Facts about the game.
+// spiel.h 참고
 const GameType kGameType{
     /*short_name=*/"kbg",
     /*long_name=*/"Ki Bo Gung",
     GameType::Dynamics::kSequential,
     GameType::ChanceMode::kDeterministic,
-    GameType::Information::kPerfectInformation,
+    GameType::Information::kImperfectInformation,
     GameType::Utility::kZeroSum,
-    GameType::RewardModel::kTerminal,
+    /* 종료시 보상 반환 */ GameType::RewardModel::kTerminal, 
     /*max_num_players=*/2,
     /*min_num_players=*/2,
     /*provides_information_state_string=*/true,
-    /*provides_information_state_tensor=*/false,
+    /*provides_information_state_tensor=*/true,
     /*provides_observation_string=*/true,
     /*provides_observation_tensor=*/true,
     /*parameter_specification=*/{}  // no parameters
@@ -30,8 +32,12 @@ REGISTER_SPIEL_GAME(kGameType, Factory);
 
 RegisterSingleTensorObserver single_tensor(kGameType.short_name);
 
+} // namespace
 
 
-}
+
+
+
+
 } // kbg
 } // open_spiel
