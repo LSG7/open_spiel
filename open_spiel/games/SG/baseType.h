edbde2f5be9f53fd09b2e5a15b092794a35d3513
LSG7 <145422695+LSG7@users.noreply.github.com>
2023-09-20 19:47:59 +0900

    필드 피스, 필드 셀, 필스 스테이트 정의
    피스 = 말정보
    셀 = 맵정보
    스테이트 = 피스 + 맵

--- open_spiel/games/SG/baseType.h
+++ open_spiel/games/SG/baseType.h
@@ -6,13 +6,36 @@
 namespace open_spiel {
 namespace baseT {
 
-enum GroundType {Normal = 0, };
+enum GroundType {Normal = 0, CannotEnter = 1, };
 
-class cell {
+struct Field_size {
+    int x; int y; int z;
+}
+struct Cell {
     GroundType ground_type;
     bool is_occupied;
+    int occupied_unit_id;
+};
+
+struct Piece {
+    bool is_alive;
     int unit_id;
+    int shift_distance;
+    float hp;
+};
+
+struct Field_pieces {
+    std::vector<Piece> allies_pieces_v;
+    std::vector<Piece> enemy_pieces_v;
 }
+
+struct Field_state {
+    Field_pieces field_pieces;
+    std::vector<Cell> field_cells_v;
+}
+
+} // baseT
+} // open_spiel
 
 
 
