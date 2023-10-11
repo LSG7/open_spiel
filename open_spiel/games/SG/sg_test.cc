665cbb1d9a5f43f630b9e891eb204c8340397141
LSG7 <145422695+LSG7@users.noreply.github.com>
2023-10-11 20:54:23 +0900

    test loadgame newinitialstate observationstring

---
+++ open_spiel/games/SG/sg_test.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "open_spiel/spiel.h"
+#include "open_spiel/spiel_utils.h"
+
+int main()
+{
+  std::cerr << "Loading game..\n" << std::endl;
+  std::shared_ptr<const open_spiel::Game> game = open_spiel::LoadGame("kbg");
+
+  if (!game) {
+    std::cerr << "problem with loading game, exiting..." << std::endl;
+    return -1;
+  }
+
+  std::cerr << "Starting new state..." << std::endl;
+  std::unique_ptr<open_spiel::State> state = game->NewInitialState();
+
+  std::cerr << "Printing observation p0..." << std::endl;
+  std::string p0_observation = state->ObservationString(0);
+
+  return 0;
+}
