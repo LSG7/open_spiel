ebd8e5eb850f82a2c5a2419879bc173cdebb68e8
LSG7 <145422695+LSG7@users.noreply.github.com>
2023-10-12 11:48:37 +0900

    BUG FIX

--- set_cmake_manual.sh
+++ set_cmake_manual.sh
@@ -1,1 +1,1 @@
-BUILD_SHARED_LIB=ON CXX=clang++ cmake -DPython3_EXECUTABLE=${which python3} -DCMAKE_CXX_COMPILER=${CXX} -DBUILD_TYPE=Debug ../open_spiel
+BUILD_SHARED_LIB=ON CXX=clang++ cmake -DPython3_EXECUTABLE=$(which python3) -DCMAKE_CXX_COMPILER=${CXX} -DBUILD_TYPE=Debug ../open_spiel
