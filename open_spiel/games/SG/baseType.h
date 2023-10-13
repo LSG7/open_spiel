a5f791539d19869a38c8c4f9425d570671194229
LSG7 <145422695+LSG7@users.noreply.github.com>
2023-10-13 14:29:45 +0900

    UnitDirection PlayerN UnitClass 추가

--- open_spiel/games/SG/baseType.h
+++ open_spiel/games/SG/baseType.h
@@ -7,8 +7,11 @@ namespace open_spiel {
 namespace baseT {
 
 enum GroundType {GT_Normal = 0, GT_CannotEnter = 1, };
+enum UnitDirection {North = 0, West = 1, South = 2, East = 3,};
+enum PlayerN {P0 = 0, P1 = 1, P2 = 2,};
+enum UnitClass {C0 = 9, C1,C2,C3,C4,C5,C6,C7,C8,C9};
 
-struct Map_coord {
+struct MapCoord {
     //z:floor y:row x:col
     int z; int y; int x;
 };
@@ -16,8 +19,6 @@ struct Cell {
     GroundType ground_type;
     bool is_occupied;
     int occupying_unit_id;
-    Map_coord coord_player0;
-    Map_coord coord_player1;
 };
 
 struct Unit {
@@ -28,8 +29,10 @@ struct Unit {
     int atk_range;
     float hp;
     float power;
-    Map_coord coord;        // 현재 위치한 셀 
-    Map_coord direction;    // coord 주위 4방향 셀  가르킨다.
+    UnitClass unit_class;
+    PlayerN player;
+    MapCoord coord;        // 현재 위치한 셀 
+    UnitDirection direction;    // coord 주위 4방향 셀  가르킨다.
     std::string name;
 };
 
