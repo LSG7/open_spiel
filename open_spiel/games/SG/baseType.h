2e09f5c68e59b78f713fedc766addfdd03034db0
LSG7 <145422695+LSG7@users.noreply.github.com>
2023-10-16 17:57:17 +0900

    actiontype

--- open_spiel/games/SG/baseType.h
+++ open_spiel/games/SG/baseType.h
@@ -10,6 +10,7 @@ enum GroundType {GT_Normal = 0, GT_CannotEnter = 1, GT_None = -1, };
 enum UnitDirection {D_North = 0, D_West = 1, D_South = 2, D_East = 3, D_None = -1,};
 enum PlayerN {P0 = 0, P1 = 1, P2 = 2, PNone = -1,};
 enum UnitClass {C0 = 9, C1,C2,C3,C4,C5,C6,C7,C8,C9, CNone = -1,};
+enum ActionType {AMove = 0, AAttk, ANone = -1,};
 
 struct MapCoord {
     //z:floor y:row x:col
