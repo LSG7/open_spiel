dc8dc93a682364aab63db39380e74630958a2260
LEESG <lsg8707@naver.com>
2024-05-19 18:34:21 +0900

    env_sg.sh 파일에 경로 추가가
    Signed-off-by: LEESG <lsg8707@naver.com>

--- env_sg.sh
+++ env_sg.sh
@@ -1,2 +1,12 @@
 export OPEN_SPIEL_HOME=${PWD}
 export LD_LIBRARY_PATH=${OPEN_SPIEL_HOME}/build_sg
+export PYTHONPATH=$PYTHONPATH:$OPEN_SPIEL_HOME:$OPEN_SPIEL_HOME/build_sg/python
+
+OS=$(uname)
+if [ "$OS" = "Darwin" ]; then
+
+
+
+else
+
+fi
